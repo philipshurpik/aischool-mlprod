323632c55ded42df6b0d061d8e73cb4472069cc0
philip <philip.shurpik@gmail.com>
2021-11-20 13:21:33 +0200

    changed im size

--- demo_profile.py
+++ demo_profile.py
@@ -18,7 +18,7 @@ def measure(use_half=True, batch_size=4, device='cpu'):
     model = torchvision.models.resnet50(pretrained=True, progress=True)
     model = model.eval().requires_grad_(False).to(dtype).to(device)
 
-    frames = torch.randn(batch_size, 3, 512, 512).to(dtype).to(device)
+    frames = torch.randn(batch_size, 3, 128, 128).to(dtype).to(device)
     activity = ProfilerActivity.CPU if device =='cpu' else ProfilerActivity.CUDA
     with profile(activities=[activity], record_shapes=True, profile_memory=True) as prof:
         with record_function("model_inference"):
@@ -30,7 +30,7 @@ def measure(use_half=True, batch_size=4, device='cpu'):
     '''
     dvc = 'cpu' if device == 'cpu' else 'cuda'
     print(prof.key_averages(group_by_input_shape=True).table(sort_by=f"{dvc}_time_total", row_limit=10))
-    print(prof.key_averages().table(sort_by=f"self_{dvc}_memory_usage", row_limit=10))
+    print(prof.key_averages(group_by_input_shape=True).table(sort_by=f"self_{dvc}_memory_usage", row_limit=10))
 
 
 def main():
