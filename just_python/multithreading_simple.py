e8ad67d74097f6a93bfe820520dd19ad564de4f2
philip <philip.shurpik@gmail.com>
2021-11-21 08:29:16 +0200

    use torch here

--- just_python/multithreading_simple.py
+++ just_python/multithreading_simple.py
@@ -1,14 +1,21 @@
 import time
 from threading import Thread
-import cv2
+# import cv2
 import numpy as np
+import torch
+import torchvision
 
-cv2.setNumThreads(1)
+model = torchvision.models.mobilenet_v3_small(pretrained=True, progress=True)
+model = model.eval().requires_grad_(False).to(torch.float32).to('cpu')
+
+# cv2.setNumThreads(1)
+torch.set_num_threads(1)
 COUNT = 50000000
 
 
 def resize(im_frames):
-    [cv2.resize(frame, (1080, 1920), interpolation=cv2.INTER_LANCZOS4) for frame in im_frames]
+    # [cv2.resize(frame, (1080, 1920), interpolation=cv2.INTER_LANCZOS4) for frame in im_frames]
+    [model(torch.tensor(frames[0]).permute(2, 0, 1).div(255).unsqueeze(0))for frame in im_frames]
 
 
 def run_single_thread(frames):
@@ -18,7 +25,7 @@ def run_single_thread(frames):
     print('Elapsed time single thread:', end - start)
 
 
-def run_multi_thread(frames, cores=4):
+def run_multi_thread(frames, cores=8):
     start = time.time()
     n = len(frames) // cores
     threads = [Thread(target=resize, args=(frames[n * i: n * (i + 1)],)) for i in range(cores)]
@@ -29,6 +36,6 @@ def run_multi_thread(frames, cores=4):
 
 
 if __name__ == '__main__':
-    frames = np.random.randint(255, size=(100, 3840, 2160, 3), dtype=np.uint8)
+    frames = np.random.randint(255, size=(80, 1280, 720, 3), dtype=np.uint8)
     run_single_thread(frames)
     run_multi_thread(frames)
