c9b5a3c29d61db1dfb7e0266b1941f9d27e42abf
philip <philip.shurpik@gmail.com>
2021-11-21 09:13:38 +0200

    add multiprocess pool

--- just_python/multithreading_simple.py
+++ just_python/multithreading_simple.py
@@ -4,6 +4,7 @@ from threading import Thread
 import numpy as np
 import torch
 import torchvision
+from multiprocessing import Pool
 
 model = torchvision.models.mobilenet_v3_small(pretrained=True, progress=True)
 model = model.eval().requires_grad_(False).to(torch.float32).to('cpu')
@@ -11,10 +12,12 @@ model = model.eval().requires_grad_(False).to(torch.float32).to('cpu')
 # cv2.setNumThreads(1)
 torch.set_num_threads(1)
 COUNT = 50000000
+CORES = 8
 
 
 def resize(im_frames):
     # [cv2.resize(frame, (1080, 1920), interpolation=cv2.INTER_LANCZOS4) for frame in im_frames]
+    print('resized', im_frames.shape)
     [model(torch.tensor(frames[0]).permute(2, 0, 1).div(255).unsqueeze(0))for frame in im_frames]
 
 
@@ -25,7 +28,7 @@ def run_single_thread(frames):
     print('Elapsed time single thread:', end - start)
 
 
-def run_multi_thread(frames, cores=8):
+def run_multi_thread(frames, cores=CORES):
     start = time.time()
     n = len(frames) // cores
     threads = [Thread(target=resize, args=(frames[n * i: n * (i + 1)],)) for i in range(cores)]
@@ -35,7 +38,19 @@ def run_multi_thread(frames, cores=8):
     print(f'Elapsed time {cores} threads:', end-start)
 
 
+def run_multi_process(frames, cores=CORES):
+    pool = Pool(processes=cores)
+    n = len(frames) // cores
+    start = time.time()
+    [pool.map_async(resize, (frames[n * i: n * (i + 1)],)) for i in range(cores)]
+    pool.close()
+    pool.join()
+    end = time.time()
+    print(f'Elapsed time {cores} processes:', end-start)
+
+
 if __name__ == '__main__':
-    frames = np.random.randint(255, size=(80, 1280, 720, 3), dtype=np.uint8)
+    frames = np.random.randint(255, size=(32, 1280, 720, 3), dtype=np.uint8)
     run_single_thread(frames)
     run_multi_thread(frames)
+    run_multi_process(frames)
