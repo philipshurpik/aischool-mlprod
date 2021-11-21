1d3c967560275aec7de1c85ff65b5e42888d9ce7
philip <philip.shurpik@gmail.com>
2021-11-21 11:19:33 +0200

    make shared array

--- just_python/multithreading_simple.py
+++ just_python/multithreading_simple.py
@@ -1,26 +1,34 @@
+import os
+
+os.environ['MKL_NUM_THREADS'] = "1"
+os.environ['OMP_NUM_THREADS'] = "1"
+
 import time
 from threading import Thread
-# import cv2
+from PIL import Image
 import numpy as np
 import torch
 import torchvision
-from multiprocessing import Pool, shared_memory
-import warnings
-warnings.filterwarnings("ignore")
+from multiprocessing import Pool, Process
+import SharedArray as sa
 
 model = torchvision.models.mobilenet_v3_small(pretrained=True, progress=True)
 model = model.eval().requires_grad_(False).to(torch.float32).to('cpu')
 
-# cv2.setNumThreads(1)
 torch.set_num_threads(1)
-COUNT = 50000000
 CORES = 8
 
 
+def resize(image, shape):
+    return np.array(Image.fromarray(image).resize(shape))
+
+
 def processing(im_frames):
-    # [cv2.processing(frame, (1080, 1920), interpolation=cv2.INTER_LANCZOS4) for frame in im_frames]
     print('processing', im_frames.shape)
-    [model(torch.tensor(frames[0]).permute(2, 0, 1).div(255).unsqueeze(0))for frame in im_frames]
+    resized = [resize(frame, (256, 256)) for frame in im_frames]
+    results = [model(torch.tensor(frames[0]).permute(2, 0, 1).div(255).unsqueeze(0))for frame in resized]
+    [resize(frame, (3840, 2160)) for frame in resized]
+    return [x.numpy().argmax() for x in results]
 
 
 def run_single_thread(frames):
@@ -51,40 +59,51 @@ def run_multi_process(frames, cores=CORES):
     print(f'Elapsed time {cores} processes:', end-start)
 
 
-def processing_shm(params):
-    shm_name, shape, start_index, end_index = params
-    existing_shm = shared_memory.SharedMemory(name=shm_name)
-    loaded = np.ndarray(shape, dtype=np.uint8, buffer=existing_shm.buf)
-    loaded_slice = loaded[start_index:end_index]
+def processing_shm(shm_name, shape, start_index, end_index):
+    loaded_slice = sa.attach(shm_name)[start_index:end_index]
     processing(loaded_slice)
-    existing_shm.close()
 
 
 def make_shared_np_array(frames):
-    shm = shared_memory.SharedMemory(create=True, size=frames.nbytes)
-    b = np.ndarray(frames.shape, dtype=frames.dtype, buffer=shm.buf)
-    b[:] = frames[:]  # Copy the original data into shared memory
-    shm.close()
-    return shm
+    name = "shm://test3"
+    shm = sa.create(name, frames.shape, dtype=np.uint8)
+    shm[:] = frames[:]
+    return name
 
 
 def run_multi_process_shm(frames, cores=CORES):
     pool = Pool(processes=cores)
-    shm = make_shared_np_array(frames)
+    shm_name = make_shared_np_array(frames)
     n = len(frames) // cores
     start = time.time()
-    [pool.map_async(processing_shm, ((shm.name, frames.shape, n * i, n * (i + 1)),)) for i in range(cores)]
+    [pool.apply_async(processing_shm, args=(shm_name, frames.shape, n * i, n * (i + 1),)) for i in range(cores)]
     pool.close()
     pool.join()
     end = time.time()
     print(f'Elapsed time {cores} shm processes:', end-start)
-    shm.unlink()
-    shm.close()
+    sa.delete(shm_name)
+
+
+def run_multi_process_shm_no_pool(frames, cores=CORES):
+    shm_name = make_shared_np_array(frames)
+    n = len(frames) // cores
+    processes = [Process(target=processing_shm, args=(shm_name, frames.shape, n * i, n * (i + 1),)) for i in range(cores)]
+    start = time.time()
+    [p.start() for p in processes]
+    [p.join() for p in processes]
+    end = time.time()
+    print(f'Elapsed time {cores} shm processes (no pool):', end-start)
+    sa.delete(shm_name)
 
 
 if __name__ == '__main__':
-    frames = np.random.randint(255, size=(48, 1280, 720, 3), dtype=np.uint8)
+    frames = np.random.randint(255, size=(32, 3840, 2160, 3), dtype=np.uint8)
     run_single_thread(frames)
+    time.sleep(2)
     run_multi_thread(frames)
+    time.sleep(2)
     run_multi_process(frames)
+    time.sleep(2)
     run_multi_process_shm(frames)
+    time.sleep(2)
+    run_multi_process_shm_no_pool(frames)
