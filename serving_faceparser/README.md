902859f69062517d27c55380abcd6aa5e0e38511
philip <philip.shurpik@gmail.com>
2021-12-12 07:09:41 +0200

    make faceparser as lib

---
+++ serving_faceparser/README.md
@@ -0,0 +1,7 @@
+# serving face_parser with different model servers
+
+### Install face_parser as package
+```
+python setup.py install
+```
+
---
+++ serving_faceparser/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+import os
+
+
+os.system("rm -rf *.egg-info && rm -rf dist && rm -rf build")
+
+setuptools.setup(
+    name='face_parser',
+    version="0.0.1",
+    author="Demo",
+    description="Package for face parser",
+    packages=setuptools.find_packages(),
+    package_data={'face_parser': ['weights/*.pth']},
+    include_package_data=True,
+
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
+
+os.system("rm -rf *.egg-info && rm -rf dist && rm -rf build")
